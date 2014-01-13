1800654ce076474fef3bcba44e91339f8eeea39c
Joe Love <joe.love@which.co.uk>
2014-01-13 15:55:19 +0000

    started js refactoring and added some css for sidebar

--- content.js
+++ content.js
@@ -8,31 +8,101 @@ function setProperty(object, keys, value) {
 }
 
 (function(){
-	MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-	var observer = new MutationObserver(function(mutations, observer) {
+	var 
+	
+	MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+
+	addTags = function(tagStructure, parentElement) {
+		var targetContainer = parentElement ? parentElement : tagContainer;
+
+		for (i in tagStructure) {
+			item = tagStructure[i];
+			
+			if (item.hasOwnProperty('className')) {
+				li = document.createElement('li');
+				li.setAttribute('class', item.className);
+
+				a = document.createElement('a');
+				a.setAttribute('class', item.aClassName);
+				a.setAttribute('title', item.aTitle);
+
+				div = document.createElement('div');
+				div.setAttribute('class', item.aClassName);
+				div.innerHTML = item.divInnerHTML.split(' - ').pop();
+
+				a.appendChild(div);
+				li.appendChild(a);
+				targetContainer.appendChild(li);
+			} else {
+				li = document.createElement('li');
+				li.style.padding = '0.25em 3em 0 1em';
+
+				div = document.createElement('div');
+				div.setAttribute('class', 'P_NAVPANEL_SUBMENU_ITEM');
+
+				icon = document.createElement('div');
+				icon.setAttribute('class', 'P_NAVPANEL_ZIPPY_ICON');
+
+				textdiv = document.createElement('div');
+				textdiv.setAttribute('class', 'P_NAVPANEL_SUBMENU_NAME');
+
+				text = document.createTextNode(i);
+
+				ul = document.createElement('div');
+				ul.setAttribute('class', 'P_NAVPANEL_SUBMENU_ITEM');
+				ul.style.padding = '0.25em 0 0 0.5em';
+
+				textdiv.appendChild(icon);
+				textdiv.appendChild(text);
+
+				div.appendChild(textdiv)
+
+				li.appendChild(div);
+				li.appendChild(ul);
+
+				targetContainer.appendChild(li);
+
+				addTags(tagStructure[i], ul);
+			}
+		}
+	},
+
+	hasMutated = function(mutations, observer){
 		var loaded = document.querySelector('.ID-tm-block'),
 			hasClass = document.querySelector('.ID-tm-block.GTM-FOLDERS'),
 			elementData = [[], [], []], elementDataLength,
-			tagStructure = {}, conditionStructure = {}, macroStructure = {},
-			tags, conditions, macros, all, allLength, element, elementList, elementListLength, a, div;
+			structureData = [{},{},{}],
+			tags, conditions, macros, blockList, blockListLength, element, elementList, elementListLength, a, div;
 
 		if(loaded && !hasClass) {
-			all = [
+			blockList = [
 				document.querySelectorAll('.P_NAVPANEL_SUBMENU .ID-tm-tags-block li'),
 				document.querySelectorAll('.P_NAVPANEL_SUBMENU .ID-tm-conditions-block li'),
 				document.querySelectorAll('.P_NAVPANEL_SUBMENU .ID-tm-macros-block li')
 			];
 
-			allLength = all.length;
-			while(--allLength >= 0) {
-				elementList = all[allLength];
+			tagTarget = '.ID-tm-tags-block';
+			conditionTarget = '.ID-tm-conditions-block';
+			macroTarget = '.ID-tm-macros-block';
+
+			tagContainer = document.querySelector(tagTarget);
+			conditionContainer = document.querySelector(conditionTarget);
+			macroContainer = document.querySelector(macroTarget);
+
+			tagContainer.innerHTML = "";
+			conditionContainer.innerHTML = "";
+			macroContainer.innerHTML = "";
+
+			blockListLength = blockList.length;
+			while(--blockListLength >= 0) {
+				elementList = blockList[blockListLength];
 				elementListLength = elementList.length;
 				while(--elementListLength >= 0) {
 					element = elementList[elementListLength];
 					a = element.getElementsByTagName('a')[0];
 					div = element.getElementsByTagName('div')[0];
-					elementData[allLength].push({
+					elementData[blockListLength].push({
 						'className' : element.className,
 						'aClassName' : a.className,
 						'aTitle' : a.title,
@@ -48,91 +118,26 @@ function setProperty(object, keys, value) {
 				});
 			}
 
-			tags = elementData[0];
-			tagsLength = tags.length;
-			while(--tagsLength >= 0) {
-				runner = tags[tagsLength];
-				treePositionData = runner.divInnerHTML.split(' - ');
-				setProperty(tagStructure, treePositionData, runner);
-			}
-
-			conditions = elementData[1];
-			conditionsLength = conditions.length;
-			while(--conditionsLength >= 0) {
-				runner = conditions[conditionsLength];
-				treePositionData = runner.divInnerHTML.split(' - ');
-				setProperty(conditionStructure, treePositionData, runner);
-			}
-
-			macros = elementData[2];
-			macrosLength = macros.length;
-			while(--macrosLength >= 0) {
-				runner = macros[macrosLength];
-				treePositionData = runner.divInnerHTML.split(' - ');
-				setProperty(macroStructure, treePositionData, runner);
-			}
-
-			tagTarget = '.ID-tm-tags-block';
-			conditionTarget = '.ID-tm-conditions-block';
-			macroTarget = '.ID-tm-macros-block';
-
-			tagContainer = document.querySelector(tagTarget);
-			conditionContainer = document.querySelector(conditionTarget);
-			macroContainer = document.querySelector(macroTarget);
-
-			tagContainer.innerHTML = "";
-			conditionContainer.innerHTML = "";
-			macroContainer.innerHTML = "";
-
-			function addTags(tagStructure, parentElement) {
-				for (i in tagStructure) {
-					item = tagStructure[i];
-					targetContainer = parentElement ? parentElement : tagContainer;
-					if (item.hasOwnProperty('className')) {
-						li = document.createElement('li');
-						li.setAttribute('class', item.className);
-
-						a = document.createElement('a');
-						a.setAttribute('class', item.aClassName);
-						a.setAttribute('title', item.aTitle);
-
-						div = document.createElement('div');
-						div.setAttribute('class', item.aClassName);
-						div.innerHTML = item.divInnerHTML.split(' - ').pop();
-
-						a.appendChild(div);
-						li.appendChild(a);
-						targetContainer.appendChild(li);
-					} else {
-						li = document.createElement('li');
-						li.setAttribute('class', 'ITEM_GROUP');
-						li.style.padding = '0.25em 3em 0 1em';
-
-						text = document.createTextNode(i);
-
-						ul = document.createElement('ul');
-						ul.setAttribute('class', 'ITEM_GROUP');
-						ul.style.padding = '0.25em 0 0 0.5em';
-
-						li.appendChild(text);
-						li.appendChild(ul);
-						targetContainer.appendChild(li);
-
-						addTags(tagStructure[i], ul);
-					}
+			for (i=0; i<=2; i++) {
+				tags = elementData[i];
+				tagsLength = tags.length;
+				while(--tagsLength >= 0) {
+					runner = tags[tagsLength];
+					treePositionData = runner.divInnerHTML.split(' - ');
+					setProperty(structureData[i], treePositionData, runner);
 				}
 			}
 
-			addTags(tagStructure, tagContainer);
-			addTags(conditionStructure, conditionContainer);
-			addTags(macroStructure, macroContainer);
+			addTags(structureData[0], tagContainer);
+			addTags(structureData[1], conditionContainer);
+			addTags(structureData[2], macroContainer);
 
 			loaded.className += ' GTM-FOLDERS';
 		}
-	});
+	},
 
-	// define what element should be observed by the observer
-	// and what types of mutations trigger the callback
+	observer = new MutationObserver(hasMutated);
+
 	observer.observe(document, {
 		subtree: true,
 		attributes: true
